f4994b400319bfa08b3a1e15f064a8e721d11c38
Lassdi <95753333+Lassdi@users.noreply.github.com>
2023-04-04 14:07:45 +0300

    Update coordinateFinder.py
    Update: kommentointi

--- src/coordinateFinder.py
+++ src/coordinateFinder.py
@@ -1,31 +1,27 @@
 import cv2
 import numpy as np
 
-# Load the image
+#Loading image
 image = cv2.imread("separatedSaturation.png")
 
-# Convert the image to grayscale
+#Greyscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Threshold the image to extract the white rectangles
+#Thresholding
 _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
-
-# Find contours of the white rectangles in the image
 contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Extract the coordinates of the bounding rectangles for each contour
+#Exctracting coordinates
 rectangles = []
 for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
     rectangles.append((x, y, x + w, y + h))
 
-# Display the rectangles
+#Displaying results
 for rectangle in rectangles:
     cv2.rectangle(image, (rectangle[0], rectangle[1]), (rectangle[2], rectangle[3]), (0, 255, 0), 2)
-
-# Show the image
 cv2.imshow("Image", image)
 cv2.waitKey(0)
 
-# Print the rectangles list
+#Printing the final list of coordinates
 print(rectangles)
